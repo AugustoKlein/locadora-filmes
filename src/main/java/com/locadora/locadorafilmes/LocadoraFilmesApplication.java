8f62656d7d6484f9c41d4144239bff7ae454298a
AugustoKlein <guto.klein7@gmail.com>
2020-09-10 13:57:20 -0300

    Commit inicial

---
+++ src/main/java/com/locadora/locadorafilmes/LocadoraFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.locadora.locadorafilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locadora/locadorafilmes/LocadoraFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locadora.locadorafilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
